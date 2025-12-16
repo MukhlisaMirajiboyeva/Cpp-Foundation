ec48270d6e56dfede6c4aef0fe9e26789e7180eb
XASAN <xasan@XASANs-MacBook-Pro.local>
2025-12-16 18:40:54 +0500

    First commit

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+ 
+   int age = 20;
+ 
+
+   std::cout<< "hello Xasan" <<std::endl;
+   cout << "hello world\n";
+   cout <<"hello tima ";
+
+cout <<" I am "<< age << "years old." ;
+int clas;
+cout << "Enter your class:" ;
+cin>> clas;
+
+cout <<" you enter class"<< clas <<"."<< endl;
+
+   return 0;
+
