4e340861d47c5c29e0b8d5d1f64a52984f2b1a5a
mubinasadullaeva <masturainagsmova@gmail.com>
2025-12-16 17:50:49 +0500

    First commit

---
+++ lesson.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b= 10;
+    int sum = a + b;
+    cout<< "The sum of " <<a << " and " << b << "is " << sum << endl;
+
+
+    int c;
+    int d;
+    cout << "ENTER TWO ENGERS:";
+    cin >> c >> d;
+    int dev = c/d;
+    std :: cout << "The division of" << c<< "by" << d << "is " << dev ;
+    return 0 ;
+}
