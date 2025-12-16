18b3bfd3655057a16a723aa21e588ded61b76d1a
Mukhlisa <mukhlisamirtajiboyeva@gmail.com>
2025-12-16 19:24:21 +0500

    Lesson 4

---
+++ Урок 4/lesson4_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP (CP_UTF8);
+    bool isWarm = true;        // На улице тепло (true)
+    bool homeworkDone = false; // Уроки не сделаны (false)
+
+    if (isWarm && homeworkDone) { // true И false = false. Условие НЕВЕРНО.
+        cout << "Иди гуляй!" << endl;
+    } else {
+        cout << "Сначала сделай уроки или подожди, пока потеплеет." << endl;
+    }
+    return 0;
+}   
+// Вывод:
+// Сначала сделай уроки или подожди, пока потеплеет.
