7dab4ce0d31452d28c0323c9b96c1b73b2e97f84
Davlatbek <bberdiyoriv324@gmail.com>
2025-12-16 17:47:51 +0500

    First commit

---
+++ part2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    cout << " Agar " << a << "ni " << b << "ga qushsak javobi " << sum << " buladi \n" ;
+
+    int c;
+    int d;
+    cin >> c >> d;
+    int dev = c % d;
+    cout << c << "ni " << d << "ga bo`lsak javobi " << dev << " buladi ";
+    return 0;
+}
