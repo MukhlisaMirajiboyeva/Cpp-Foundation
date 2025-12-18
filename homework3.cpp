65866a7dba92b986dd34010e16bfb1f56ed7bddc
mubinasadullaeva <masturainagsmova@gmail.com>
2025-12-18 22:23:59 +0500

    дз

---
+++ homework3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    double num1 ;
+    double num2 ;
+    char operation ;
+
+    cout << "enter first nember.";
+    cin >> num1;
+
+    if (operation == '+') {
+        cout << "Result:" << num1 + num2 << endl;
+    } else if (operation == '-') {
+        //num1 - num2
+    } else {
+        cout << "Error: Invalid operator." << endl;
+    }
+
+    return 0;
+}
