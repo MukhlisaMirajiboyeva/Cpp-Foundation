10d0805b68210371fe2a6fc4fdaca3f7f2783e1e
Davlatbek <bberdiyoriv324@gmail.com>
2026-01-06 17:35:20 +0500

    Zadacha 1

---
+++ Урок_8/Homework.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+int main () {
+    SetConsoleOutputCP (CP_UTF8);
+    for (int i = 1; i <= 10; i++) {
+        if (i == 4) {
+            cout << "Попускаем ящик 4! \n";
+            continue;
+        }
+        if (i == 7) {
+            cout << "Опасность! Проверка остановлена! \n";
+            break;
+        }
+    }
+    return 0;
+}
