bb1df9f0819cf2237f7711de9d2e8414a0c45d26
Davlatbek <bberdiyoriv324@gmail.com>
2026-01-22 17:57:27 +0500

    Project1

--- Project1.cpp
+++ Project1.cpp
@@ -6,48 +6,113 @@
 #include <windows.h>
 using namespace std;
 
-void changeStat(int* stat, int delta) {
-    *stat += delta;
-}
+int main() {
+    SetConsoleOutputCP (CP_UTF8);
+    int heroHP = 120;
+    int bossHP = 150;
 
-int main () {
-    SetConsoleOutputCP (65001);
-    srand(time(0));
+    bool hasMinions = false;
 
-    int heroHP = 1627;
-    int bossHP = 3465;
     int phases[] = {10, 20, 30};
-    int* currectDmd = phases;
+    int* currentDmg = phases;
+
     int* minions = nullptr;
+    int minionCount = 3;
+
+    vector<string> history;
 
     while (heroHP > 0 && bossHP > 0) {
         int attack;
-        cout << "1 - базавая атака \n2 - критическая атака оглашаешего врага\nВыбор: ";
+        cout << "\nВыбери атаку:\n";
+        cout << "1 - Обычная атака (10 урона)\n";
+        cout << "2 - Сильная атака (20 урона)\n";
+        cout << "3 - Спецприём (15 урона + оглушение)\n";
+        cout << "Твой выбор: ";
         cin >> attack;
 
+        int heroDmg = 0;
+        bool stun = false;
+
         if (attack == 1) {
-            bossHP -= 227;
-            cout << "Вы атакавали босса. НР босса: " << bossHP << endl;
+            heroDmg = 10;
+            history.push_back("Герой использует обычную атаку");
         }
         else if (attack == 2) {
-            bossHP -= 524;
-            cout << "Вы нанесли критический удар. НР босса: " << bossHP << endl;
-            if (bossHP > 0){
-                continue;
+            heroDmg = 20;
+            history.push_back("Герой использует сильную атаку");
+        }
+        else if (attack == 3) {
+            heroDmg = 15;
+            stun = true;
+            history.push_back("Герой использует спецприём и оглушает босса");
+        }
+        else {
+            cout << "Неверный выбор! Ход пропущен.\n";
+            history.push_back("Герой ошибся с выбором атаки");
+            continue;
+        }
+
+       
+        bossHP -= heroDmg;
+        cout << "HP босса: " << bossHP << endl;
+
+        
+        if (bossHP <= 50 && !hasMinions) {
+            history.push_back("Босс призывает миньонов");
+
+            minions = new int[minionCount];
+            for (int i = 0; i < minionCount; i++)
+                minions[i] = 5;
+
+            hasMinions = true;
+
+            currentDmg++;
+            history.push_back("Босс переходит в новую фазу");
+        }
+
+        
+        if (bossHP <= 0) {
+            history.push_back("Босс побеждён");
+            break;
+        }
+
+       
+        
+        if (stun) {
+            history.push_back("Босс пропускает ход из-за оглушения");
+            continue; 
+        }
+
+        
+        heroHP -= *currentDmg;
+        history.push_back(
+            "Босс атакует героя (-" + to_string(*currentDmg) + " HP)"
+        );
+        cout << "HP героя: " << heroHP << endl;
+
+        
+        if (hasMinions) {
+            for (int i = 0; i < minionCount; i++) {
+                heroHP -= minions[i];
+                history.push_back(
+                    "Миньон " + to_string(i + 1) +
+                    " наносит " + to_string(minions[i]) + " урона"
+                );
             }
         }
-        if (bossHP <= 0){
-            cout << "Босс певержен!\n";
-            break; 
-        }
-        heroHP -= 368;
-        cout << "Вас атакавал босс. Ваше здаровье: " << heroHP << endl;
     }
-    if (heroHP <= 0) {
-        cout << "Вы проиграли!\n";
-    }
-    else { cout << "Вы выиграли!\n";
-    }
-    return 0;
 
+    
+    if (minions != nullptr) {
+        delete[] minions;
+        minions = nullptr;
+    }
+
+    
+    cout << "\n=== ИСТОРИЯ БОЯ ===\n";
+    for (int i = 0; i < history.size(); i++) {
+        cout << i + 1 << ". " << history[i] << endl;
+    }
+
+    return 0;
 }
