dad9a714a1af7408e8024054f535e0865e623acd
VictoriaX-D <greykakuldasheva@gmail.com>
2026-01-29 19:39:19 +0500

    задания

---
+++ lesson11-20/ex18.1.py
@@ -0,0 +1,9 @@
+student = {
+    "имя": "алекс",
+    "возраст": 18,
+    "курс": "python"
+}
+print(student["имя"])
+student["возраст"] = 19
+student["город"] = "ташкент"
+print(student)
---
+++ lesson11-20/ex18.py
@@ -0,0 +1,8 @@
+heroes = ["железный человек", "тор", "халк", "человек паук", "капитан америка"]
+first = heroes[0]
+last = heroes[-1]
+team_a = heroes[1:3]
+team_b = heroes[-3:]
+print(f"первый: {first}, последний: {last}")
+print(f"команда А: {team_a}")
+print(f"команда Б:{team_b}")
