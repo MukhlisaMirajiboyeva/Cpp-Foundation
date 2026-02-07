91228c352e295363e6ac36edb654ab66ce91d81d
Mukhlisa <mukhlisamirtajiboyeva@gmail.com>
2026-02-07 20:27:53 +0500

    project 5: crypto

---
+++ Project_Crypto/key_collector.py
@@ -0,0 +1,16 @@
+# key_collector.py
+
+def assemble_key():
+    final_phrase = ""
+    
+    # ЗАДАЧА: Напиши цикл, который пройдет по числам от 1 до 3.
+    # Внутри цикла формируй имя файла: f"parts/part{i}.txt"
+    # Открывай каждый файл, читай его содержимое и добавляй в final_phrase.
+    
+    # ТВОЙ КОД ЗДЕСЬ:
+
+    print(f"Собранная фраза: {final_phrase}")
+
+if __name__ == "__main__":
+    assemble_key()
+    
