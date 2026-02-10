6116bc1d21e1d33b7311c313411bd187a8b6446f
Pozitiv41k <temurkiller876@gmail.com>
2026-02-10 18:21:11 +0500

    OMEGA

---
+++ Cpp-Foundation-project-1-omega/Project_OMEGA/Level_1/login.py
@@ -0,0 +1,31 @@
+# Level_1/login.py
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import system_core
+
+def check_password(password):
+    # ЗАДАНИЕ: Исправьте эту функцию!
+    # Пароль администратора: "admin_omega"
+    # Если пароль верный - функция должна вернуть True
+    # Сейчас она всегда возвращает False :(
+    
+    if password == "admin_omega":
+        print("Пароль верный...")
+        return True # <--- ОШИБКА ЗДЕСЬ
+    else:
+        return False
+
+# --- НЕ МЕНЯЙТЕ КОД НИЖЕ ---
+print("--- СИСТЕМА БЕЗОПАСНОСТИ OMEGA ---")
+user_pass = input("Введите пароль доступа: ")
+
+if check_password(user_pass):
+    print("\n[ACCESS GRANTED]")
+    print("Доступ разрешен.")
+    system_core.add_points(50)
+    print("СЛЕДУЮЩИЙ ШАГ: Найдите в папке проекта скрытую директорию: 'secure_folder_88'")
+else:
+    print("\n[ACCESS DENIED]")
+    print("Неверный пароль или ошибка в протоколе безопасности.")
